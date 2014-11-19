4fe5d0329722a812fbc82d4d18ee956b508f4450
senta <senta118@gmail.com>
2014-11-19 23:21:47 +0900

    set ruby-mode to Berksfile

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -286,6 +286,7 @@
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfle" . ruby-mode))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
