121c316a7f52e24a909c126892f4ea6cfac9a26e
senta <senta118@gmail.com>
2015-09-25 06:07:39 +0900

    change setup messages

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 for name in *; do
   target="$HOME/.$name"
   if [ -e "$target" ] && [ ! -L "$target" ]; then
-      echo "WARNING: $target already exists but it's not a symlink."
+      echo "WARNING: $target already exists however not a symlink"
   else
     if [ "$name" != 'setup.sh' ] && [ "$name" != 'README.md' ]; then
-      echo "Creating $target"
+      echo "creating $target"
       rm "$target"
       ln -s "$PWD/$name" "$target"
     fi
