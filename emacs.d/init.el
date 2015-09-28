f623435f514a4854b9d6cdb5d94bfe5170b9f795
senta <senta118@gmail.com>
2015-09-29 01:12:12 +0900

    setup ggtags to support .jsx file as javascript

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -596,6 +596,8 @@
 (add-hook 'c-mode-hook 'helm-gtags-mode)
 (add-hook 'c++-mode-hook 'helm-gtags-mode)
 (add-hook 'asm-mode-hook 'helm-gtags-mode)
+(add-hook 'web-mode-hook 'helm-gtags-mode)
+(add-hook 'js-mode-hook 'helm-gtags-mode)
 
 (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
 (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
