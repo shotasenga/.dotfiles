e1ba365cb11ef1ebece1beba88dc46a796b302d3
senta <senta118@gmail.com>
2015-12-02 23:45:03 +0900

    bin mouse action to ggtag
    Bind "Command + click" to go to definision

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -653,6 +653,7 @@
 (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
 (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
 (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key global-map (kbd "<s-mouse-1>") 'helm-gtags-dwim)
 (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
 (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
 (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
