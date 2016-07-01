9ade04db484c7e858ba12548f0ada6e397492ec9
s.senga <1989.shota@gmail.com>
2016-07-01 23:10:12 +0900

    add site local fook for init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -838,3 +838,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; user site local
+(load (expand-file-name "local.el" user-emacs-directory) 'no-error)
