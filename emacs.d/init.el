f4b647c886206cb4057976863e49354b2d904099
senta <1989.shota@gmail.com>
2016-09-02 12:36:02 +0900

    Emacs js2-mode on .babelrc

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -516,6 +516,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; @ js2-mode
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.babelrc$" . js2-mode))
 (setq js2-basic-offset 2)
 (add-hook 'js-mode-hook 'js2-minor-mode)
 
