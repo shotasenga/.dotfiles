9e4c82e48f3fed14e8a0aa4e68d36b53b995b0ca
Shota Senga <shota@senta.me>
2019-12-16 06:06:04 -0800

    feat(emacs) change theme to doom-dracula
    This is also the first step of the transition to literate configuration

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -1,4 +1,21 @@
+;; Configure package repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; load use-package here which is used to use 
+(unless (or (package-installed-p 'use-package)
+            (package-installed-p 'diminish))
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (package-install 'diminish))
+
+
 ;; TODO: create and load config.org though org-babel
+;; load literate configuration
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
 
 ;; load legacy config file
 ;; TODO: delete after declutter the config
--- common/emacs.d/legacy.el
+++ common/emacs.d/legacy.el
@@ -12,19 +12,19 @@
 (add-to-list 'exec-path "/usr/local/bin")
 
 
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; @ package
-(when (require 'package nil t)
-  ;; add repositry MELPA
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.milkbox.net/packages/"))
-  ;; add repositry ELPA
-  (add-to-list 'package-archives
-               '("ELPA" . "http://tromey.com/elpa/"))
-  ;; Marmalade
-  (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (package-initialize))
+;; (when (require 'package nil t)
+;;   ;; add repositry ELPA
+;;   (add-to-list 'package-archives
+;;                '("ELPA" . "http://tromey.com/elpa/"))
+;;   ;; Marmalade
+;;   (add-to-list 'package-archives
+;;                '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;   (package-initialize))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -42,7 +42,6 @@
                       coffee-mode
                       dash
                       deft
-                      dracula-theme
                       emmet-mode
                       epl
                       exec-path-from-shell
@@ -174,9 +173,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; @ theme & visual
-(load-theme 'dracula t)
-
-
 ;; show line numbers
 (global-linum-mode t)
 (setq linum-format "%5d ")
@@ -212,7 +208,7 @@
   )
 
 ;; powerline
-(setq ns-use-srgb-colorspace nil)
+;(setq ns-use-srgb-colorspace nil)
 (require 'powerline)
 (powerline-default-theme)
 
@@ -306,29 +302,29 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; @ whitespace
 ;; for whitespace-mode
-(require 'whitespace)
-;; see whitespace.el for more details
-(setq whitespace-style '(face tabs tab-mark spaces space-mark))
-(setq whitespace-display-mappings
-      ;;      '((space-mark ?\u3000 [?\u25a1]) ; zenkaku-whitespace to white-square-mark
-      '(
-        ;; (space-mark 32 [46] [46]) ; 32 SPACE, 183 MIDDLE DOT ·, 46 FULL STOP .
-        ;; (newline-mark [10 13] [182]) ; 10 LINE FEED
-        ;; WARNING: the mapping below has a problem.
-        ;; When a TAB occupies exactly one column, it will display the
-        ;; character ?\xBB at that column followed by a TAB which goes to
-        ;; the next TAB column.
-        ;; If this is a problem for you, please, comment the line below.
-        (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
+;; (require 'whitespace)
+;; ;; see whitespace.el for more details
+;; (setq whitespace-style '(face tabs tab-mark spaces space-mark))
+;; (setq whitespace-display-mappings
+;;       ;;      '((space-mark ?\u3000 [?\u25a1]) ; zenkaku-whitespace to white-square-mark
+;;       '(
+;;         ;; (space-mark 32 [46] [46]) ; 32 SPACE, 183 MIDDLE DOT ·, 46 FULL STOP .
+;;         ;; (newline-mark [10 13] [182]) ; 10 LINE FEED
+;;         ;; WARNING: the mapping below has a problem.
+;;         ;; When a TAB occupies exactly one column, it will display the
+;;         ;; character ?\xBB at that column followed by a TAB which goes to
+;;         ;; the next TAB column.
+;;         ;; If this is a problem for you, please, comment the line below.
+;;         (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
 ;;(setq whitespace-space-regexp "\\(\u3000+\\)")
 ;;      aaa <- this line tabbed
-(set-face-foreground 'whitespace-tab "#505050")
-(set-face-background 'whitespace-tab 'nil)
-(set-face-underline  'whitespace-tab t)
-(set-face-foreground 'whitespace-space "#505050")
-(set-face-background 'whitespace-space 'nil)
-(set-face-bold-p 'whitespace-space t)
-(global-whitespace-mode 1)
+;; (set-face-foreground 'whitespace-tab "#505050")
+;; (set-face-background 'whitespace-tab 'nil)
+;; (set-face-underline  'whitespace-tab t)
+;; (set-face-foreground 'whitespace-space "#505050")
+;; (set-face-background 'whitespace-space 'nil)
+;; (set-face-bold-p 'whitespace-space t)
+;(global-whitespace-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; @ isearch region
@@ -749,92 +745,6 @@
 
 
 (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "f38b00afa9c773ee3d0d597241abf13439467a9ea1628a6d71d51a1810f7ffc3" "a939897b56010ef16d737b3a145ab3f935e0da6122ded3bf9cad28f88b09fd68" "02414c4cfbbe9805b89a5ec66d3d3deb4ae1e4795ed2092eab240ca0cb79ea96" "303488aa27ce49f658a7ba4035e93380421e394ec2799ae8fd952d08808c7235" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(flymake-google-cpplint-command "/usr/local/bin/cpplint")
- '(fringe-mode 6 nil (fringe))
- '(linum-format " %5d ")
- '(package-selected-packages
-   (quote
-    (htmlize rainbow-mode rjsx-mode helm-ag ag emojify markdown-toc lua-mode yasnippet yaml-mode web-mode stylus-mode slime sass-mode rich-minority restclient redo+ powerline php-mode org multiple-cursors migemo markdown-mode magit json-mode js2-mode jade-mode iedit highlight-indentation helm-projectile helm-gtags helm-emmet helm-descbinds go-mode git-gutter ggtags flymake-google-cpplint flymake-cursor expand-region exec-path-from-shell deft coffee-mode blank-mode autopair auto-complete-c-headers ace-jump-mode)))
- '(powerline-color1 "#00779a")
- '(powerline-color2 "#00475a")
- '(sml/mode-width
-   (if
-       (eq powerline-default-separator
-           (quote arrow))
-       (quote right)
-     (quote full)))
- '(sml/pos-id-separator
-   (quote
-    (""
-     (:propertize " " face powerline-active1)
-     (:eval
-      (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s" powerline-default-separator
-                            (car powerline-default-separator-dir)))
-                   (quote powerline-active1)
-                   (quote powerline-active2))))
-     (:propertize " " face powerline-active2))))
- '(sml/pos-minor-modes-separator
-   (quote
-    (""
-     (:propertize " " face powerline-active1)
-     (:eval
-      (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s" powerline-default-separator
-                            (cdr powerline-default-separator-dir)))
-                   (quote powerline-active1)
-                   nil)))
-     (:propertize " " face sml/global))))
- '(sml/pre-id-separator
-   (quote
-    (""
-     (:propertize " " face sml/global)
-     (:eval
-      (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s" powerline-default-separator
-                            (car powerline-default-separator-dir)))
-                   nil
-                   (quote powerline-active1))))
-     (:propertize " " face powerline-active1))))
- '(sml/pre-minor-modes-separator
-   (quote
-    (""
-     (:propertize " " face powerline-active2)
-     (:eval
-      (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s" powerline-default-separator
-                            (cdr powerline-default-separator-dir)))
-                   (quote powerline-active2)
-                   (quote powerline-active1))))
-     (:propertize " " face powerline-active1))))
- '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 
 ;; user site local
 (load (expand-file-name "local.el" user-emacs-directory) 'no-error)
