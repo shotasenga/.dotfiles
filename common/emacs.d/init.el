0f24e4e2183f58f1417afd9c776c68b12154db5e
Shota Senga <shota@senta.me>
2019-12-17 09:38:47 -0800

    feat(emacs) try and which-key
    https://cestlaz.github.io/posts/using-emacs-1-setup/

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -1,5 +1,6 @@
 ;; Configure package repositories
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
