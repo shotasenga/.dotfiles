2882ce2c36ed4db6326037bb312d92058f9f80b8
Shota Senga <shota@senta.me>
2019-12-23 00:47:19 -0800

    Revert "feat(emacs) move package repository config onto config.org"
    This reverts commit b500d69255860780e7146b7ae5ab5907bbe24695.
    
    I have to have package cofnigured before evaluate config.org to
    use newr version of org-mode.

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -1,7 +1,23 @@
-;; load literate configuration :)
+;; Configure package repositories
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; load use-package here which is used to use 
+(unless (or (package-installed-p 'use-package)
+            (package-installed-p 'diminish))
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (package-install 'diminish))
+
+
+;; TODO: create and load config.org though org-babel
+;; load literate configuration
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 
-;; load the legacy config file
+;; load legacy config file
 ;; TODO: delete after declutter the config
 (load-file (expand-file-name "legacy.el" user-emacs-directory))
