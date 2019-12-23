b500d69255860780e7146b7ae5ab5907bbe24695
Shota Senga <shota@senta.me>
2019-12-23 00:12:51 -0800

    feat(emacs) move package repository config onto config.org

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -1,23 +1,7 @@
-;; Configure package repositories
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(package-initialize)
-
-;; load use-package here which is used to use 
-(unless (or (package-installed-p 'use-package)
-            (package-installed-p 'diminish))
-  (package-refresh-contents)
-  (package-install 'use-package)
-  (package-install 'diminish))
-
-
-;; TODO: create and load config.org though org-babel
-;; load literate configuration
+;; load literate configuration :)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 
-;; load legacy config file
+;; load the legacy config file
 ;; TODO: delete after declutter the config
 (load-file (expand-file-name "legacy.el" user-emacs-directory))
