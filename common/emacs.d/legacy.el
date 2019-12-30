7f5c17bf183e18b0bc2b0d95ad812af6c29fb5eb
Shota Senga <shota@senta.me>
2019-12-30 02:16:11 -0800

    feat(emacs) ggtags
    Since I've found Dumb Jump and it seems work well enough. I disabled
    ggtags for now.

--- common/emacs.d/legacy.el
+++ common/emacs.d/legacy.el
@@ -42,7 +42,6 @@
                       flymake-cursor
                       flymake-easy
                       flymake-google-cpplint
-                      ggtags
                       haml-mode
                       json-reformat
                       json-snatcher
@@ -259,53 +258,6 @@
   )
 (put 'upcase-region 'disabled nil)
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; @ gtag and helm
-;; (require 'ggtags)
-;; (add-hook 'c-mode-common-hook
-;;           (lambda ()
-;;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-;;               (ggtags-mode 1))))
-
-;; (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
-;; (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
-;; (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
-;; (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
-;; (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
-;; (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
-
-;; (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
-
-;; (setq
-;;  helm-gtags-ignore-case t
-;;  helm-gtags-auto-update t
-;;  helm-gtags-use-input-at-cursor t
-;;  helm-gtags-pulse-at-cursor t
-;;  helm-gtags-prefix-key "\C-cg"
-;;  helm-gtags-suggested-key-mapping t
-;;  )
-
-;; (require 'helm-gtags)
-;; ;; Enable helm-gtags-mode
-;; (add-hook 'dired-mode-hook 'helm-gtags-mode)
-;; (add-hook 'eshell-mode-hook 'helm-gtags-mode)
-;; (add-hook 'c-mode-hook 'helm-gtags-mode)
-;; (add-hook 'c++-mode-hook 'helm-gtags-mode)
-;; (add-hook 'asm-mode-hook 'helm-gtags-mode)
-;; (add-hook 'web-mode-hook 'helm-gtags-mode)
-;; (add-hook 'js-mode-hook 'helm-gtags-mode)
-
-;; (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-;; (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-;; (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-;; (define-key global-map (kbd "<s-mouse-1>") 'helm-gtags-dwim)
-;; (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-;; (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-;; (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-
-
-;; (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
 
 ;; ;; user site local
 ;; (load (expand-file-name "local.el" user-emacs-directory) 'no-error)
