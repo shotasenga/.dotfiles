66794aa681b3bfdb9aa3c7134745623cda982b2d
Shota <shota.senga@thrive.health>
2021-09-13 11:05:33 -0700

    mac: remove some unused applications from installation

--- macos.sh
+++ macos.sh
@@ -94,8 +94,6 @@ brew cask install dropbox
 brew cask install firefox
 brew cask install google-chrome
 brew cask install hammerspoon
-brew cask install hyperswitch
-brew cask install karabiner-elements
 brew cask install linear
 brew cask install mysqlworkbench
 brew cask install processing
