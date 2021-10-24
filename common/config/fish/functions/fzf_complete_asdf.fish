fc47e8a159c4f177f47560887d63eb6b6a2acb1d
Shota Senga <shota.senga0123@gmail.com>
2021-10-23 22:07:13 -0700

    fish: fzf copmletion for asdf

---
+++ common/config/fish/functions/fzf_complete_asdf.fish
@@ -0,0 +1,12 @@
+function fzf_complete_asdf
+    set -l subcmd $argv[2]
+
+    switch $subcmd
+        case install
+            asdf list-all $argv[3] | fzf -m +s --tac -q "$argv[4..]"
+        case uninstall
+            asdf list $argv[3] | fzf -m +s --tac -q "$argv[4..]"
+        case '*'
+            return
+    end
+end
