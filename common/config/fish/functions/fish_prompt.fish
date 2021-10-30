278d98e8ca9ccaa03f00fa70552de0b0b11a477f
Shota Senga <shota.senga0123@gmail.com>
2021-10-29 22:36:57 -0700

    fish: use starship for prompt

--- common/config/fish/functions/fish_prompt.fish
+++ common/config/fish/functions/fish_prompt.fish
@@ -1,30 +1,40 @@
 function fish_prompt
-  # set monkey randomly
-  set __r (random)
-  if [ (math $__r '%7') -eq 1 ]
-    printf '🐵'
-  else if [ (math $__r '%3') -eq 1 ]
-    printf '🙈'
-  else if [ (math $__r '%2') -eq 1 ]
-    printf '🙊'
-  else
-    printf '🙉'
-  end
+    if command -q starship
+        starship init fish | source
+        commandline -f repaint
+    else
+        __fish_prompt_monkey
+    end
+end
 
-  # show cwd
-  set_color 81a1c1
-  printf ' %s' (prompt_pwd)
 
-  # show git infomation
-  set_color normal
-  printf '%s ' (__fish_git_prompt)
-  
-  if git rev-parse --is-inside-work-tree > /dev/null 2>&1
-    printf '[%s] ' (__fish_git_prompt_informative_status)
-  end
+function __fish_prompt_monkey
+    # set monkey randomly
+    set __r (random)
+    if [ (math $__r '%7') -eq 1 ]
+        printf '🐵'
+    else if [ (math $__r '%3') -eq 1 ]
+        printf '🙈'
+    else if [ (math $__r '%2') -eq 1 ]
+        printf '🙊'
+    else
+        printf '🙉'
+    end
 
-  # show FISH
-  set_color -o ebcb8b
-  printf '⋊> '
-  set_color normal
+    # show cwd
+    set_color 81a1c1
+    printf ' %s' (prompt_pwd)
+
+    # show git infomation
+    set_color normal
+    printf '%s ' (__fish_git_prompt)
+
+    if git rev-parse --is-inside-work-tree > /dev/null 2>&1
+        printf '[%s] ' (__fish_git_prompt_informative_status)
+    end
+
+    # show FISH
+    set_color -o ebcb8b
+    printf '⋊> '
+    set_color normal
 end
