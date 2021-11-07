86c193a3f05e821c33be080364a7541a84f96d44
Shota Senga <shota.senga0123@gmail.com>
2021-11-07 01:06:51 -0700

    setup: add macos module

---
+++ install-macos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOT_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+
+source $DOT_DIR/utils.sh
+
+# pacman -Syy
+# sudo pacman -S --needed tree htop jq
+module fish
+module git
+module asdf
+module emacs
+module tmux
+module nvim
+module scripts
+module macos
