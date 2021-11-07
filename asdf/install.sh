bde8a2fd1450db3cc4fcc973490f433e6bfd4528
Shota Senga <shota.senga0123@gmail.com>
2021-11-07 00:22:44 -0700

    asdf: add missing config for direnv

--- asdf/install.sh
+++ asdf/install.sh
@@ -8,8 +8,12 @@ source $MODULE_ROOT/../utils.sh
 
 link_config $MODULE_ROOT/asdf/envrc "${HOME}/.envrc"
 link_config $MODULE_ROOT/asdf/tool-versions "${HOME}/.tool-versions"
+mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/asdf"
+link_config $MODULE_ROOT/default-python-packages "${XDG_CONFIG_HOME:-$HOME/.config}/default-python-packages"b
 
-if [[ "$SHELL" != *fish ]]; then
-    # TODO: move the script to this module directory from fish/functions
-    echo 'run "asdf_install_version" to install dependencies'
-fi
+link_config $MODULE_ROOT/direnv "${XDG_CONFIG_HOME:-$HOME/.config}/direnv"
+
+# TODO: move the script to this module directory from fish/functions
+echo 'run "asdf_install_version" to install dependencies'
+
+
