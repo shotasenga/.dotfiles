8ffd1550e8e3b6860cca81b0a99bd88b89fee1de
Shota Senga <shota.senga0123@gmail.com>
2023-06-19 10:45:50 -0700

    setup: fix macos.sh
    - run `brew bundle` instead of echoing
    - add `fish` to `/etc/fish` when needed
    - correct config paths
    - allow local config for fish (ignore them)

--- macos.sh
+++ macos.sh
@@ -12,17 +12,17 @@ else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo brew bundle --file $DOT_DIR/Brewfile
+brew bundle --file $DOT_DIR/Brewfile
 
 # WARN: this replaces the target files if exist
 ln -Ffhs $DOT_DIR/git/git "${XDG_CONFIG_HOME:-$HOME/.config}/git"
 ln -Ffhs $DOT_DIR/fish/fish "${XDG_CONFIG_HOME:-$HOME/.config}/fish"
 ln -Ffhs $DOT_DIR/emacs/emacs.d "${HOME}/.emacs.d"
-ln -Ffhs $DOT_DIR/nvim "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
-ln -Ffhs $DOT_DIR/hammerspoon "${HOME}/hammerspoon"
-ln -Ffhs $DOT_DIR/bin "${HOME}/.bin"
-ln -Ffhs $DOT_DIR/tmux/tmux.conf "${HOME}/.tmux.conf"
-ln -Ffhs $DOT_DIR/tmux/tmux.mac.conf "${HOME}/.tmux.env.conf"
+ln -Ffhs $DOT_DIR/nvim/nvim "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+ln -Ffhs $DOT_DIR/macos/hammerspoon "${HOME}/hammerspoon"
+ln -Ffhs $DOT_DIR/scripts/bin "${HOME}/.bin"
+ln -Ffhs $DOT_DIR/tmux/tmux/tmux.conf "${HOME}/.tmux.conf"
+ln -Ffhs $DOT_DIR/tmux/tmux/tmux.mac.conf "${HOME}/.tmux.env.conf"
 
 # TODO: consider Nix
 # ln -Ffhs $DOT_DIR/asdf/asdf/envrc "${HOME}/.envrc"
@@ -36,7 +36,7 @@ ln -Ffhs $DOT_DIR/tmux/tmux.mac.conf "${HOME}/.tmux.env.conf"
 # Post install
 # - fish as default shell
 FISH_BIN=$(brew --prefix)/bin/fish
-if grep -q $FISH_BIN /etc/shells; then
+if ! grep -q $FISH_BIN /etc/shells; then
     sudo bash -c "echo ${FISH_BIN} /etc/shells"
 fi
 
@@ -63,3 +63,5 @@ if [ ! -f ~/.ssh/id_ed25519 ]; then
     ssh-keygen -t ed25519 -C "${USER}@${HOSTNAME}"
     gh auth login
 fi
+
+echo COMPLETED 🎉
