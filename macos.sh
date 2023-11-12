dd3990caf13b87bef94f8ce667fa216ff76b93e8
Shota Senga <shota.senga0123@gmail.com>
2023-11-11 17:54:35 -0800

    mac: brew is not in the path right after the installation

--- macos.sh
+++ macos.sh
@@ -10,6 +10,7 @@ if [ $(which brew) ]; then
     echo brew update
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew bundle --file $DOT_DIR/Brewfile
