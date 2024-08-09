4d8398f9e8a11787ac46d65acd34a6cf002fd636
Shota Senga <shota.senga0123@gmail.com>
2024-08-09 00:10:53 -0700

    fish: preload `autojump` function to use my own `j` command
    loading the fish script with `source` doesn't work anymore. It
    seems I need to load `autojump` through Fish's function loading
    mechanism.

--- fish/conf.d/50-fasd.fish
+++ fish/conf.d/50-fasd.fish
@@ -1,8 +1,4 @@
-if test -f /opt/homebrew/share/autojump/autojump.fish
-  source /opt/homebrew/share/autojump/autojump.fish
-else if test -f /usr/local/share/autojump/autojump.fish
-  source /usr/local/share/autojump/autojump.fish
-end
+type autojump 2>&1 > /dev/null # preload autojump.fish to prevent overriding `j`
 
 function j -d "jump to a directory"
     set -l path (autojump -s dot 2>&1|grep -E '^[0-9\.]+:\s+/'|sort -nr|cut -d (echo -n \t) -f 2 | fzf -q "$argv" -1 -0 --no-sort +m )
