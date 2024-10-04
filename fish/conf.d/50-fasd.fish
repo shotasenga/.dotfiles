86dff2cee658cb96bc71395bdd92a28c387f78b6
Shota Senga <shota.senga0123@gmail.com>
2024-10-04 09:35:28 -0700

    fish: preload `autojump` script
    re: 4d8398f9e8a11787ac46d65acd34a6cf002fd636
    
    `type autojump` doesn't load the script anymore. Explicit loading is
    required.

--- fish/conf.d/50-fasd.fish
+++ fish/conf.d/50-fasd.fish
@@ -1,4 +1,9 @@
-type autojump 2>&1 > /dev/null # preload autojump.fish to prevent overriding `j`
+# preload autojump.fish to prevent overriding `j`
+if test -f /opt/homebrew/share/autojump/autojump.fish
+  source /opt/homebrew/share/autojump/autojump.fish
+else if test -f /usr/local/share/autojump/autojump.fish
+  source /usr/local/share/autojump/autojump.fish
+end
 
 function j -d "jump to a directory"
     set -l path (autojump -s dot 2>&1|grep -E '^[0-9\.]+:\s+/'|sort -nr|cut -d (echo -n \t) -f 2 | fzf -q "$argv" -1 -0 --no-sort +m )
