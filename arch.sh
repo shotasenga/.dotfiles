a275351ee63572e1357176a762fd59386aadbcb6
Shota Senga <shota.senga0123@gmail.com>
2025-12-27 15:37:55 -0800

    syncthing: switch back to syncthing

--- arch.sh
+++ arch.sh
@@ -100,7 +100,7 @@ sudo pacman -S --needed ttf-hack
 
 
 # Other applications
-yay -S --needed maestral anki-official-binary-bundle google-chrome slack-desktop
+yay -S --needed syncthing anki-official-binary-bundle google-chrome slack-desktop
 
 
 echo COMPLETED 🎉
