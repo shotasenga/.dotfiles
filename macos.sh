e62fe205e91ca6db9ff21784d54caa4bd14a2fbd
Shota Senga <shota.senga0123@gmail.com>
2026-07-25 09:32:35 -0700

    macos: OS config

--- macos.sh
+++ macos.sh
@@ -97,7 +97,27 @@ ln -Tfs $DOT_DIR/zed/settings.json "${XDG_CONFIG_HOME:-$HOME/.config}/zed/settin
 
 
 # OS preference
+# use `defaults delete <application> [config]` to go back to the default
 defaults write -g NSMenuEnableActionImages -bool NO # no icons https://daringfireball.net/2026/03/what_to_do_about_those_menu_item_icons_in_macos_26_tahoe
-
+# Flash screen on `beep`
+defaults write com.apple.universalaccess flashScreen -bool true
+# Dock: size = smallest, maginification = on, position = left, autohide = on
+defaults write com.apple.dock tilesize -int 16
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock largesize -int 128
+defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock autohide -bool true
+# Trackpad: natural scrolling = off
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+# Finder: show extensions, show hidden files, show path bar
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+# Keyboard: fast repeat, auto period = off, smart quotes = off
+defaults write NSGlobalDomain KeyRepeat -int 3
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
+defaults write NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSAutomaticQuoteSubstitutionEnabled -bool false
 
 echo COMPLETED 🎉
